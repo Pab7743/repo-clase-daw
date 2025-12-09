5c59d268b7e0f2f74cc93fc49d989392ec71a30d
Pablo <pablo@example.com>
2025-12-09 11:08:16 +0100

    hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,9 @@
 <?php
-	//Realizafo por Pablo
-	//El nombre por defecto es mundo
+
+	//Realizafo por: Pablo pablo@ejemplo.com
+	//El nombre por defecto es "mundo"
+	require('Hola Mundo.php');
+	print "Introduce tu nombre";
 	$nombre=trim(fgets(STDIN));
-	@print "Hola {$nombre}\n";
+	print new HolaMundo($nombre);
 ?>
