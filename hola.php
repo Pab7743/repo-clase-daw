393e20fafcc024e3f5e9ff52ec75b0f339343dcd
Pablo <pablo@example.com>
2025-12-09 10:16:40 +0100

    Este commit esta mal

--- hola.php
+++ hola.php
@@ -1,5 +1,5 @@
 <?php
-	//El nombre por defecto es mundo
-	$nombre= isset($arg[1]) ? $arg[1] : "Mundo";	
+	//Cambiamos de nuevo el comentario
+	$nombre= isset($arg[1]) ? $arg[1] : "Mundo";
 	@print "Hola {$nombre}\n";
 ?>
